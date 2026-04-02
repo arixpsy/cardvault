70a25a1c9480607251327ad2d99f736c07ef32c1
Phua Si Yu Arix <arixgg@gmail.com>
2026-04-03 02:45:46 +0800

    feat: Search Card

--- apps/frontend/src/pages/Collection.vue
+++ apps/frontend/src/pages/Collection.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { Plus } from "@lucide/vue"
-import Button from "../components/Common/Button.vue"
 import PageContent from "../components/Common/PageContent.vue"
 import PageHeader from "../components/Common/PageHeader.vue"
+import AddCardButton from "../components/Collection/AddCardButton.vue"
 </script>
 
 <template>
   <PageContent>
     <PageHeader title="Collection" description="122 Cards">
-      <Button><Plus />Add Card</Button>
+      <AddCardButton />
     </PageHeader>
   </PageContent>
 </template>
