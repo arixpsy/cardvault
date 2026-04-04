e7e86f81c8b5cdb5848750785a4602db027df73b
Phua Si Yu Arix <arixgg@gmail.com>
2026-04-04 23:17:44 +0800

    tweak: add expense form layout

--- apps/frontend/src/components/Expenses/AddExpenseButton.vue
+++ apps/frontend/src/components/Expenses/AddExpenseButton.vue
@@ -18,55 +18,65 @@ const closePanel = () => (open.value = false)
   <Button @click="openPanel"> <Plus />Add Expense</Button>
 
   <SlidePanel title="Add Expense" :open="open" @close="closePanel">
-    <div class="expense-form">
-      <FormGroup>
-        <FormLabel>Item Name</FormLabel>
-        <FormInput placeholder="E.g Booster Pack, ETB"></FormInput>
-      </FormGroup>
+    <form>
+      <div class="expense-inputs">
+        <FormGroup>
+          <FormLabel>Item Name</FormLabel>
+          <FormInput placeholder="E.g Booster Pack, ETB"></FormInput>
+        </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Category</FormLabel>
-        <FormSelect>
-          <option></option>
-        </FormSelect>
-      </FormGroup>
+        <FormGroup>
+          <FormLabel>Category</FormLabel>
+          <FormSelect>
+            <option></option>
+          </FormSelect>
+        </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Amount ($)</FormLabel>
-        <FormInput placeholder="0.00" type="number"></FormInput>
-      </FormGroup>
+        <FormGroup>
+          <FormLabel>Amount ($)</FormLabel>
+          <FormInput placeholder="0.00" type="number"></FormInput>
+        </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Date</FormLabel>
-        <FormInput placeholder="0.00" type="date"></FormInput>
-      </FormGroup>
+        <FormGroup>
+          <FormLabel>Date</FormLabel>
+          <FormInput placeholder="0.00" type="date"></FormInput>
+        </FormGroup>
 
-      <FormGroup>
-        <FormLabel>Notes</FormLabel>
-        <FormTextarea />
-      </FormGroup>
+        <FormGroup>
+          <FormLabel>Notes</FormLabel>
+          <FormTextarea />
+        </FormGroup>
+      </div>
 
-      <div class="form-footer">
+      <div class="footer">
+        <Button type="button" variant="secondary" @click="closePanel">Cancel</Button>
         <Button>Add Expense</Button>
       </div>
-    </div>
+    </form>
   </SlidePanel>
 </template>
 
 <style lang="css" scoped>
-.expense-form {
+form {
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.expense-inputs {
   display: flex;
   flex-direction: column;
   gap: 14px;
-  height: 100%;
-  overflow: auto;
 }
 
-.form-footer {
+.footer {
   position: sticky;
   bottom: 0;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
   padding-top: 12px;
   background: var(--surface);
 }
