0f64170cbd771a1abe86944de82e5e3d2979e53f
Phua Si Yu Arix <arixgg@gmail.com>
2026-04-06 01:06:07 +0800

    debug: cloudfare fe build

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vite.dev/config/
+console.log('CLERK KEY:', process.env.VITE_CLERK_PUBLISHABLE_KEY)
+
 export default defineConfig({
   plugins: [vue()],
   server: {
